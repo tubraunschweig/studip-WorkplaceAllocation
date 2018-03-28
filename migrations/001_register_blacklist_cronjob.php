f22d8996940c07796fc083a6c49ceabe0325254b
Julian Jacobi <mail@julianjacobi.net>
2018-03-28 12:06:56 +0200

    update cronjob registration

--- migrations/001_register_blacklist_cronjob.php
+++ migrations/001_register_blacklist_cronjob.php
@@ -10,26 +10,26 @@
  * Time: 11:01
  */
 
+require_once '../cronjobs/blacklist_cronjob.php';
+
 class RegisterBlacklistCronjob extends Migration {
 
     private $FILENAME;
 
-    public function __construct()
+    public function __construct($verbose = false)
     {
+        parent::__construct($verbose);
         $this->FILENAME = str_replace('/migrations', '', __DIR__) . '/cronjobs/blacklist_cronjob.php';
 
         $this->FILENAME = substr($this->FILENAME, strpos($this->FILENAME, 'public/'));
     }
 
     public function up(){
-        $task_id = CronjobScheduler::registerTask($this->FILENAME, true);
-
-        CronjobScheduler::schedulePeriodic($task_id, 20, 0);
+        BlacklistCronJob::register()->schedulePeriodic(20, 0);
     }
 
     public function down()
     {
-        $task_id = CronjobTask::findOneByFilename($this->FILENAME)->task_id;
-        CronjobScheduler::unregisterTask($task_id);
+        BlacklistCronJob::unregister();
     }
 }
